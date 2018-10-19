1aa008cc324bfcf2f0612658565e3a0e59f1e8e3
VictorGil <victorgilsanz@gmail.com>
2018-10-19 20:55:23 +0200

    changes to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// this is our first component
+import { HeroesComponent } from './heroes/heroes.component';
+
+import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component'; // <-- NgModel lives here
+
+// @NgModule.declarations array.
 @NgModule({
   declarations: [
-    AppComponent
+    // The component classes must be declared here
+    AppComponent,
+    // this is a custom component
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
