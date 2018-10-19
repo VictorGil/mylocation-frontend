b96876aad01ca694f73e22ac335e7a74e93cae1d
VictorGil <victorgilsanz@gmail.com>
2018-10-19 20:54:54 +0200

    changes to the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
+// it imports the Component symbol from the Angular core library
 import { Component } from '@angular/core';
 
+// this is an annotation
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  // this is a property inside the AppComponent class
+  title = 'Tour of Heroes';
 }
