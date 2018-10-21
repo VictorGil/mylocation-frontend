dcbd0cf53e90ec84970a6edd6e26e047073c5372
gabete <gabete@gmail.com>
2018-10-21 19:35:57 +0200

    Adding LocationDataJson class and using it when constructing a LocationData object

--- src/app/locationdata.ts
+++ src/app/locationdata.ts
@@ -1,9 +1,35 @@
+import { LocationDataJson } from './locationdatajson';
+
 export class LocationData {
-    latitude: string;
-    longitude: string;
-    horizontalAccuracy: string;
-    altitude: string;
-    verticalAccuracy: string;
-    timeChecked: number;
-    timeMeasured: number;
+    readonly latitude: string;
+    readonly longitude: string;
+    readonly horizontalAccuracy: string;
+
+    readonly altitude: string;
+    readonly verticalAccuracy: string;
+
+    readonly timeChecked: number;
+    readonly timeCheckedString: string;
+    readonly timeMeasured: number;
+    readonly timeMesasuredString: string;
+
+    constructor(locationDataJson: LocationDataJson) {
+        this.latitude = locationDataJson.latitude;
+        this.longitude = locationDataJson.longitude;
+        this.horizontalAccuracy = locationDataJson.horizontalAccuracy;
+
+        this.altitude = locationDataJson.altitude;
+        this.verticalAccuracy = locationDataJson.verticalAccuracy;
+
+        this.timeChecked = locationDataJson.timeChecked;
+        const timeCheckedDate: Date = new Date(0);
+        timeCheckedDate.setUTCSeconds(this.timeChecked);
+        this.timeCheckedString = timeCheckedDate.toString();
+
+        this.timeMeasured = locationDataJson.timeMeasured;
+        const timeMeasuredDate: Date = new Date(0);
+        timeMeasuredDate.setUTCSeconds(this.timeMeasured);
+        this.timeMesasuredString = timeMeasuredDate.toString();
+    }
 }
+
---
+++ src/app/locationdatajson.ts
@@ -0,0 +1,11 @@
+export class LocationDataJson {
+    readonly latitude: string;
+    readonly longitude: string;
+    readonly horizontalAccuracy: string;
+
+    readonly altitude: string;
+    readonly verticalAccuracy: string;
+
+    readonly timeChecked: number;
+    readonly timeMeasured: number;
+}
