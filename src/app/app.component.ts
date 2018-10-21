a67635dc7b656a7413c6b7d301dd3977c8530f62
VictorGil <victorgilsanz@gmail.com>
2018-10-21 12:02:34 +0200

    Changing the title in the app.component.html from 'Tour of Heroes' to 'My location'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   // this is a property inside the AppComponent class
-  title = 'Tour of Heroes';
+  title = 'My location';
 }
