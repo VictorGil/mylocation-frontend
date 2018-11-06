fc93859f69f745862dae048c01750c520280d3b9
VictorGil <victorgilsanz@gmail.com>
2018-11-06 22:40:34 +0100

    removing yet another reference to angular-tour-of-heroes this time from app.e2e-spec.ts file

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to angular-tour-of-heroes!');
+    expect(page.getParagraphText()).toEqual('Welcome to mylocation!');
   });
 });
